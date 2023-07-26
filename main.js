cd1e045e8dbb7ea860b07f30423c171033117736
Bapa Bozinovich <bapa.bc04@gmail.com>
2023-07-26 17:07:29 -0400

    Added clase13.htm to index.html, styles.css and main.js. Also it is included the action between the different menus

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
+const shoppingCartMenuIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+shoppingCartMenuIcon.addEventListener('click', toggleShoppingCartAside);
 
 function toggleDesktopMenu() {
+  mobileMenu.classList.add('inactive');
+  aside.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+  desktopMenu.classList.add('inactive');
+  aside.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
+
+function toggleShoppingCartAside() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  aside.classList.toggle('inactive');
+} 
